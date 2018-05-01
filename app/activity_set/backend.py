dee110e8bd9acf34a4ec061643f4a02010304383
liuzunrui <liuzunrui@163.com>
2018-05-01 22:21:09 +0800

    活动相关

--- app/activity_set/backend.py
+++ app/activity_set/backend.py
@@ -1,8 +1,9 @@
 # -*- coding:utf-8 -*-
 
 # __author__ = 'liuzunrui'
-
+import datetime
 from utils.fileUtil.readfile import ExcelUtil
+from utils.DbUtils.dbutil import dbutil_select, insert_db
 
 
 def get_test_data(filename, sheetname):
@@ -10,3 +11,144 @@ def get_test_data(filename, sheetname):
     print "../filedata/" + filename + ".xls", (u'%s' % sheetname)
     test_data = data.get_data()
     return test_data
+
+
+def get_award_code(data_tmp):
+    '''
+    根据规则生成奖励编号
+    :param data_tmp:
+    :return:
+    '''
+    if data_tmp == '9999999999':
+        _award_code = '0000000001'
+    elif data_tmp != '9999999999':
+        _award_code = str(int(data_tmp) + 1)
+        s_0 = ''
+        for i in range(0, 10-len(_award_code)):
+            s_0 += '0'
+        _award_code = s_0 + _award_code
+    _award_code = datetime.datetime.now().strftime('%Y%m%d%H%M%S%f')[0:8] + _award_code
+    return _award_code
+
+
+def get_lottery_code(data_tmp):
+    '''
+    根据规则生成抽奖编号
+    :return:
+    '''
+    if data_tmp == '9999999999':
+        _lottery_code = '0000000001'
+    elif data_tmp != '9999999999':
+        _lottery_code = str(int(data_tmp) + 1)
+        s_0 = ''
+        for i in range(0, 10 - len(_lottery_code)):
+            s_0 += '0'
+        _lottery_code = s_0 + _lottery_code
+    _lottery_code = datetime.datetime.now().strftime('%Y%m%d%H%M%S%f')[0:8] + _lottery_code
+    return _lottery_code
+
+
+def get_prize_code(data_tmp):
+    '''
+    根据规则生成 奖励编号
+    :param data_tmp:
+    :return:
+    '''
+
+    if data_tmp == '9999999999':
+        _prize_code = '0000000001'
+    elif data_tmp != '9999999999':
+        _prize_code = str(int(data_tmp) + 1)
+        s_0 = ''
+        for i in range(0, 10 - len(_prize_code)):
+            s_0 += '0'
+        _prize_code = s_0 + _prize_code
+    _prize_code = datetime.datetime.now().strftime('%Y%m%d%H%M%S%f')[0:8] + _prize_code
+    return _prize_code
+
+
+def get_award_name(test_data):
+    award_name_list = []
+    for i in range(0, len(test_data)):
+        award_name = test_data[i][u'奖品名称']
+        award_name_list.append(award_name)
+    return award_name_list
+
+
+def get_award_type(test_data):
+    award_type_list = []
+    for i in range(0, len(test_data)):
+        award_type = test_data[i][u'奖励类型']
+        award_type_list.append(award_type)
+    return award_type_list
+
+
+def get_award_level(test_data):
+    award_level_list = []
+    for i in range(0, len(test_data)):
+        award_level = int(test_data[i][u'奖励等级'])
+        award_level_list.append(award_level)
+    return award_level_list
+
+
+def get_wallet_amount(test_data):
+    '''
+    获取红包金额， 如果是实物，则取和其他不同的规则编号即可
+    :return:
+    '''
+    wallet_amount_list = []
+    for i in range(0, len(test_data)):
+        wallet_amount = int(test_data[i][u'红包金额']) * 100
+        wallet_amount_list.append(wallet_amount)
+    return wallet_amount_list
+
+
+def get_wallet_rule_code(test_data):
+    '''
+    根据红包金额。找对应的红包规则
+    :return:
+    '''
+    wallet_amount_list = get_wallet_amount()
+    wallet_rule_code_list = []
+    for i in range(0, len(test_data)):
+        wallet_rule_code_sql = "select rule_code from HD_WALLET_RULE T WHERE T.TOTAL_AMOUNT = %d" \
+                               % wallet_amount_list[i]
+        wallet_rule_code = dbutil_select(wallet_rule_code_sql)[0]
+        wallet_rule_code_list.append(wallet_rule_code)
+    return wallet_rule_code_list
+
+
+def get_prize_count(test_data):
+    prize_count_list = []
+    for i in range(0, len(test_data)):
+        prize_count = int(test_data[i][u'奖项份数'])
+        prize_count_list.append(prize_count)
+    return prize_count_list
+
+
+def get_win_probability(test_data):
+    win_probability_list = []
+    for i in range(0, len(test_data)):
+        win_probability = int(test_data[i][u'中奖概率（%）'])
+        win_probability_list.append(win_probability)
+    return win_probability_list
+
+
+def create_lottery_award_tmp(test_data, prize_code,
+                             lottery_code, create_time,
+                             update_time, create_people,
+                             update_people, version,
+                             goods_name, platform_id):
+    '''
+    创建抽奖奖项
+    :return:
+    '''
+    for i in range(0, len(test_data)):
+        insert_lottery_award_sql = "insert into HD_LOTTERY_SETTING VALUES ('%s', '%s', '%s', '%s', %d, %d, '$%s', " \
+                                   "'%s', '%s', '%s', %d, '%s', '%s', %d, '%s')" \
+                                   % (prize_code, lottery_code, get_award_type()[i], get_wallet_rule_code()[i],
+                                      get_prize_count()[i], get_prize_count()[i], create_time, update_time,
+                                      create_people, update_people, version, get_award_level()[i], goods_name,
+                                      get_win_probability()[i], platform_id)
+        print insert_lottery_award_sql
+        insert_db(insert_lottery_award_sql)
--- app/activity_set/views.py
+++ app/activity_set/views.py
@@ -5,8 +5,10 @@ import datetime
 from django.shortcuts import render, redirect, HttpResponse
 
 from backend import get_test_data
-from constants import AREA_TYPE, area_code, NAME_FILTER_FLAG, IS_RANDOM
+from constants import AREA_TYPE, area_code, NAME_FILTER_FLAG, IS_RANDOM, AWARD_TYPE, IS_DOUBLE, IS_LOTTERY
 from utils.DbUtils.dbutil import dbutil_select, insert_db
+from backend import get_award_code, get_lottery_code, get_prize_code
+from backend import create_lottery_award_tmp
 
 # Create your views here.
 
@@ -45,6 +47,11 @@ def file_list(request):
 
 
 def create_activity(request):
+    '''
+    创建活动
+    :param request:
+    :return:
+    '''
     if request.method == 'GET':
         return render(request, "hello")
     elif request.method == 'POST':
@@ -112,8 +119,148 @@ def create_activity(request):
         select_sql = "select * from hd_activity a WHERE a.activity_code ='%s'" \
                      % activity_code
         print select_sql
-    return render(request, 'msg: sucess')
+    return render(request, 'msg: success')
 
 
 def create_activity_award(request):
-    pass
+    '''
+    活动奖励
+    :param request:
+    :return:
+    '''
+    if request.method == 'GET':
+        return render(request, 'hello award')
+    elif request.method == 'POST':
+        filename = request.POST['filename']
+        sheetname = request.POST['sheetname']
+        test_data = get_test_data(filename=filename, sheetname=sheetname)
+        # 抽奖编号：找到当前最大的值 取出后十位，若后十位为9999999999 则抽奖编号为0000000001
+        award_code_select = "select MAX(t.award_code) from HD_ACTIVITY_AWARD_SETTING t"
+        award_code_tmp = dbutil_select(award_code_select)[0][-10:]
+        award_code = get_award_code(award_code_tmp)
+        award_name = test_data[0][u'奖励名称']
+        award_type = AWARD_TYPE[test_data[0][u'奖励类型']]
+        _award_amount = int(test_data[0][u'奖励金额']) * 100
+        rule_code_select_sql = "select t.rule_code from HD_WALLET_RULE t WHERE t.total_amount = %d" % _award_amount
+        rule_code = dbutil_select(rule_code_select_sql)[0]
+        condition_code = test_data[0][u'参与条件']
+        is_lottery = IS_LOTTERY[test_data[0][u'是否参与抽奖']]
+        award_desc = ''
+        award_count = 99999
+        remainder = 99999
+        per_max_count = test_data[0][u'单人最大奖励份数']
+        create_time = datetime.datetime.now().strftime('%Y%m%d%H%M%S%f')[0:17]
+        update_time = create_time
+        create_people = 'S00000000000150'
+        update_people = 'S00000000000150'
+        activity_code = int(test_data[0][u'活动编号'])
+
+        reg_real_count = test_data[0][u'特惠数字']  # 互金留存活动中用来作为留存天数的字段
+        if reg_real_count == '':
+            reg_real_count = 0
+        else:
+            reg_real_count = int(reg_real_count)
+        amt_minimum = int(test_data[0][u'金额下限']) * 100
+        amt_maximum = int(test_data[0][u'金额上限']) * 100
+
+        is_double = IS_DOUBLE[test_data[0][u'是否连击翻倍']]
+        print is_double
+        double_condition_lm = 0
+        award_amount = int(test_data[0][u'奖励金额']) * 100
+        double_rule_code = test_data[0][u'翻倍条件']
+
+        lottery_num = test_data[0][u'抽奖次数']
+
+        if lottery_num == '':
+            lottery_num = 0
+        else:
+            lottery_num = int(lottery_num)
+
+        insert_award_sql = "insert into HD_ACTIVITY_AWARD_SETTING(" \
+                           "AWARD_CODE, AWARD_NAME, AWARD_TYPE,RULE_CODE, CONDITION_CODE, IS_LOTTERY, AWARD_DESC, " \
+                           "AWARD_COUNT, REMAINDER, PER_MAX_COUNT, CREATE_TIME, UPDATE_TIME, CREATE_PEOPLE, " \
+                           "UPDATE_PEOPLE, ACTIVITY_CODE, REG_REAL_COUNT, AMT_MINIMUN, AMT_MAXIMUN, IS_DOUBLE, " \
+                           "DOUBLE_CONDITION_LM, AWARD_AMOUNT, DOUBLE_RULE_CODE, LOTTERY_NUM) " \
+                           "VALUES ('%s', '%s', '%s', '%s', '%s', '%s'," \
+                           " '%s', %d, %d, %d, '%s', '%s', '%s', '%s', '%s', %d, %d, %d, '%s', %d," \
+                           " %d, '%s', %d)" % (award_code, award_name, award_type, rule_code, condition_code,
+                                               is_lottery, award_desc,award_count, remainder, per_max_count,
+                                               create_time, update_time, create_people, update_people, activity_code,
+                                               reg_real_count, amt_minimum, amt_maximum, is_double, double_condition_lm,
+                                               award_amount, double_rule_code, lottery_num)
+
+        print insert_award_sql
+        insert_db(insert_award_sql)
+    return redirect(request, "alert")
+
+
+def create_lottery(request):
+    '''
+    抽奖设置
+    :param request:
+    :return:
+    '''
+    if request.method == 'GET':
+        return render(request, 'hello lottery')
+    elif request.method == 'POST':
+        filename = request.POST['filename']
+        sheetname = request.POST['sheetname']
+        test_data = get_award_code(filename, sheetname)
+        lottery_code_select = "select MAX(t.LOTTERY_CODE) from HD_LOTTERY t"
+        lottery_code_tmp = dbutil_select(lottery_code_select)[0][-10:]
+        lottery_code = get_lottery_code(lottery_code_tmp)
+        lottery_name = test_data[0][u'抽奖名称']
+        activity_code = int(test_data[0][u'活动编号'])
+        start_time_select_sql = "select t.start_time from hd_activity t where t.activity_code = '%s'" % activity_code
+        end_time_select_sql = "select t.end_time from hd_activity t where t.activity_code = '%s'" % activity_code
+        start_time = dbutil_select(start_time_select_sql)[0]
+        end_time = dbutil_select(end_time_select_sql)[0]
+        status = '0'
+        create_time = datetime.datetime.now().strftime('%Y%m%d%H%M%S%f')[0:17]
+        update_time = create_time
+        create_people = 'S00000000000150'
+        update_people = 'S00000000000150'
+        probablitity_flag = '1'
+        insert_lottery_sql = "insert into hd_lottery VALUES (" \
+                             "'%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')" % (
+                             lottery_code, lottery_name, start_time, end_time, status, activity_code, create_time,
+                             update_time, create_people, update_people, probablitity_flag)
+        print insert_lottery_sql
+        insert_db(insert_lottery_sql)
+    return redirect(request, "lottery")
+
+
+def create_lottery_award(request):
+    '''
+    抽奖奖项设置
+    :param request:
+    :return:
+    '''
+    if request.method == 'GET':
+        return render(request, 'lottery_award')
+    elif request.method == 'POST':
+        filename = request.POST['filename']
+        sheetname = request.POST['sheetname']
+        test_data = get_test_data(filename, sheetname)
+        prize_code_select = "select MAX(T.prize_code) from HD_LOTTERY_SETTING T"
+        prize_code_tmp = dbutil_select(prize_code_select)[0][-10:]
+        activity_code = int(test_data[0][u'活动编号'])
+        prize_code = get_prize_code(prize_code_tmp)
+        lottery_code_sql = "select T.lottery_code from HD_LOTTERY T WHERE T.activity_code = '%s'" % activity_code
+        lottery_code = dbutil_select(lottery_code_sql)[0]
+        version = 0
+        platform_id = '0001'
+        create_time = datetime.datetime.now().strftime('%Y%m%d%H%M%S%f')[0:17]
+        update_time = create_time
+        create_people = 'S00000000000150'
+        update_people = 'S00000000000150'
+        goods_name = ''
+        create_lottery_award_tmp(
+            test_data, prize_code,
+            lottery_code, create_time,
+            update_time, create_people,
+            update_people, version,
+            goods_name, platform_id
+        )
+
+    return render(request, "lottery_award")
